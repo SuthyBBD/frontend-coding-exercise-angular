898d09c949eb8cc8bd5d2516f47d5fa073ed0d9d
Michael Sutherland <michael.sutherland@absa.co.za>
2019-08-05 11:58:34 +0200

    Map component not included in commit (whoops)

---
+++ frontend/app/shared/map/map.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {LocationCoordinates} from '../model/LocationCoordinates';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  @Input() currentLocation: LocationCoordinates;
+  @Input() deliveryLocation: LocationCoordinates;
+  @Input() vendorLocation: LocationCoordinates;
+  @Input() deliveredAt: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
